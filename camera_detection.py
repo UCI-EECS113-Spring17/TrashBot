7e04dbbe2870e22f1e2fe9d54cce6a010414ecbb
Jun Li <junl14@uci.edu>
2017-06-08 18:14:35 -0700

    Create camera_detection.py

--- camera_detection.py
+++ camera_detection.py
@@ -1,38 +1,38 @@
-%matplotlib inline
+
 import cv2
 from matplotlib import pyplot as plt
 import numpy as np
-from pynq.board import Button
 from pynq import Overlay
 Overlay("base.bit").download()
 
 cap = cv2.VideoCapture(0)
-obj_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi',fourcc, 5, (640,480))
-if not cap.isOpened():
-    print('Camera is not open.')
+obj_cascade = cv2.CascadeClassifier('haarcascade_shoe.xml')
+#fourcc = cv2.VideoWriter_fourcc(*'XVID')
+#out = cv2.VideoWriter('output.avi',fourcc, 5, (640,480))
+if cap.isOpened():
+	while True:
+		ret, frame = cap.read()
+		if ret:
+			gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+			detection = obj_cascade.detectMultiScale(gray, 1.3, 5)
+			if len(detection) != 0:
+				print('Detect.')
+				'''
+				for (x, y, w, h) in detection:
+					cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+				'''
+				# detection location x, y
+				loc_x = detection[0][0] + detection[0][2] / 2
+				loc_y = detection[0][1] + detection[0][3] / 2				
+							
+			else:
+				print('Not detect')
 
-while cap.isOpened():
-    if Button(0).read():
-        break
-
-    ret, frame = cap.read()
-    if ret:
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        detection = obj_cascade.detectMultiScale(gray, 1.3, 5)
-        if len(detection) != 0:
-            print('Detect.')
-            for (x, y, w, h) in detection:
-                cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-        else:
-            print('Not detect')
-        
-        out.write(frame)
+			#out.write(frame)
             
 print('stop')
 cap.release()
-out.release()
+#out.release()
 
 
 
